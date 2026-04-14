33ec601630b91ccd3b8df460be7a3c3ea153e7cb
Uğur Ege Çelik <uguregecel@gmail.com>
2026-04-14 21:32:02 +0300

    Insert customers data

---
+++ sql/04_insert_data_part3_customers.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customers(FullName, City, Email) VALUES
+('Ahmet Yılmaz', 'İstanbul', 'ahmet@gmail.com'),
+('Mehmet Demir', 'Ankara', 'mehmet@gmail.com'),
+('Ayşe Kaya', 'İzmir', 'ayse@gmail.com'),
+('Fatma Çelik', 'Bursa', 'fatma@gmail.com'),
+('Ali Veli', 'Antalya', 'ali@gmail.com');
