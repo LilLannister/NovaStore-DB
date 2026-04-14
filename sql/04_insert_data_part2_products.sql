34eca117d0b918509285e1b03b795a0a7209d451
Uğur Ege Çelik <uguregecel@gmail.com>
2026-04-14 21:29:52 +0300

    Insert products data with category relations

---
+++ sql/04_insert_data_part2_products.sql
@@ -0,0 +1,11 @@
+INSERT INTO Products (ProductName, Price, Stock, CategoryID) VALUES
+('iPhone 14', 45000, 10, 1),
+('Laptop', 30000, 5, 1),
+('T-Shirt', 300, 50, 2),
+('Jean Pantolon', 800, 20, 2),
+('Roman Kitap', 150, 40, 3),
+('Bilim Kitabı', 200, 25, 3),
+('Parfüm', 1200, 15, 4),
+('Krem', 250, 30, 4),
+('Kahve Makinesi', 5000, 8, 5),
+('Sandalye', 700, 12, 5);
