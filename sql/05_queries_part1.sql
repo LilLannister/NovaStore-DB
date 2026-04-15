d814ef5cfc38e5c4d2b0131cc86f4ad5033533db
Uğur Ege Çelik <uguregecel@gmail.com>
2026-04-15 20:14:39 +0300

    Add join query for customers and orders

--- sql/05_queries_part1.sql
+++ sql/05_queries_part1.sql
@@ -2,3 +2,14 @@ SELECT ProductName, Stock
 FROM Products
 WHERE Stock<20
 ORDER BY Stock DESC;
+GO
+
+SELECT
+    Customers.FullName,
+    Customers.City,
+    Orders.OrderDate,
+    Orders.TotalAmount
+FROM Customers
+INNER JOIN Orders
+    ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Orders.OrderDate;
