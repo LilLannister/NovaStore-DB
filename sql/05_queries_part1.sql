cd0ba06f3e91dd1f033529b56cff232f5e26b00a
Uğur Ege Çelik <uguregecel@gmail.com>
2026-04-15 20:23:40 +0300

    Add multi join query for customer product details

--- sql/05_queries_part1.sql
+++ sql/05_queries_part1.sql
@@ -13,3 +13,20 @@ FROM Customers
 INNER JOIN Orders
     ON Customers.CustomerID=Orders.CustomerID
 ORDER BY Orders.OrderDate;
+GO
+
+SELECT 
+    Customers.FullName,
+    Products.ProductName,
+    Products.Price,
+    Categories.CategoryName
+FROM Customers
+INNER JOIN Orders
+    ON Customers.CustomerID=Orders.CustomerID
+INNER JOIN OrderDetails
+    ON Orders.OrderID=OrderDetails.OrderID
+INNER JOIN Products
+    ON OrderDetails.ProductID=Products.ProductID
+INNER JOIN Categories
+    ON Products.CategoryID=Categories.CategoryID
+WHERE Customers.FullName = 'Ahmet Yılmaz';
